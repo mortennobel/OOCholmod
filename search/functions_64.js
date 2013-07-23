c909a512b9888f7983ab7a5825abc4969d93dbe0
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2013-07-23 13:21:02 +0200

    Update documentation

--- search/functions_64.js
+++ search/functions_64.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['densematrix',['DenseMatrix',['../classoocholmod_1_1_dense_matrix.html',1,'oocholmod']]],
   ['dropsmallentries',['dropSmallEntries',['../classoocholmod_1_1_sparse_matrix.html#a37193ab45b4d0860eff9a30f4e6c8cf5',1,'oocholmod::SparseMatrix']]]
 ];
---
+++ search/functions_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['norm',['norm',['../classoocholmod_1_1_dense_matrix.html#aab6bfaad86da393429839a768c0b0232',1,'oocholmod::DenseMatrix::norm()'],['../classoocholmod_1_1_sparse_matrix.html#aa281049cb8b723fcfb49de5bce0d9807',1,'oocholmod::SparseMatrix::norm()']]]
+];
